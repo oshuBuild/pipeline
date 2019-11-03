9b090c9a0e5d247a7735bbafcc7ddfb6171de2b6
oshuBuild <mac@172.21.11.7>
2019-11-03 14:07:56 +0200

    part is added

---
+++ index2.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var app = express();
+var port = 3700;
+app.get("/", function(req, res){
+    res.send("hello world");
+});
+app.listen(port);
+module.exports = app;
+console.log("Listening on port " + port);
